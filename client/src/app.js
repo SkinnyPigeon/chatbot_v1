aed60c177d2d3e51c41148ed7991e5c2f3ddac99
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-10 21:45:45 +0100

    sorted the file structure

--- client/src/app.js
+++ client/src/app.js
@@ -1,71 +1,79 @@
 var tmi = require( 'tmi.js' );
-var Calculator = require( './models/Calculator' );
+
 var Cards = require( './models/Cards' );
+var Game = require( './models/Game' );
 
 var options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        cluster: 'aws',
-        reconnect: true
-    },
-    identity: {
-        username: "RoboBleepBloop",
-        password: "oauth:if3e2ziho9n2bdoy053vie45w2g1t3"
-    },
-    channels: [ 'skinnypigeon' ]
+	options: {
+		debug: true
+	},
+	connection: {
+		cluster: 'aws',
+		reconnect: true
+	},
+	identity: {
+		username: "RoboBleepBloop",
+		password: "oauth:if3e2ziho9n2bdoy053vie45w2g1t3"
+	},
+	channels: [ 'skinnypigeon' ]
 };
 
-var client = new tmi.client(options);
+var client = new tmi.client( options );
 
 client.connect();
 
 client.on( 'connected', function( address, port ) {
-    client.action( "skinnypigeon I am connected to your chat" );
-})
-
-// client.on( 'chat', function( channel, user, message, self ) {
-//     var calc = new Calculator();
-//     calc.sum( message );
-//     var answer = calc.equals();
-//     client.action( "skinnypigeon", answer );
-//     console.log( answer );
-// });
-
-// client.on( 'chat', function( channel, user, message, self ) {
-//     var pack = new Cards();
-//     var card1 = pack.deal();
-//     var card2 = pack.deal();
-//     var cards = card1 + card2;
-
-//     client.action( "skinnypigeon", cards );
-// });
-
-client.on("message", function (channel, userstate, message, self) {
-    if (self) return;
-
-    switch(userstate["message-type"]) {
-        case "action":
-            break;
-        case "chat":
-            if( message === "!deal" ) {
-                deal();
-        }
-            break;
-        case "whisper":
-            break;
-        default:
-            break;
-    }
+	client.action( "skinnypigeon I am connected to your chat" );
 });
 
-var deal = function() {
-    var pack = new Cards();
-    var card1 = pack.deal();
-    var card2 = pack.deal();
-    var cards = card1 + card2;
+client.on( 'message', function( channel, userstate, message, self ) {
 
-    client.action( "skinnypigeon", cards );
+	if( self ) return;
+
+	switch( userstate[ 'message-type' ]) {
+		case "action":
+			break;
+		case "chat":
+			playGame( message, userstate );
+			break;
+		case "whisper":
+			break;
+		default:
+			break;
+	}
+
+});
+
+var playGame = function( message, userstate ) {
+	console.log( userstate );
+	switch( message ) {
+		case "!play":
+			play( userstate )
+		case "!deal":
+			deal();
+			break
+		default:
+			break
+	}
+};
+
+var deal = function() {
+	var pack = new Cards;
+	var card1 = pack.deal();
+	var card2 = pack.deal();
+	var cards = card1 + card2;
+
+	client.action( "skinnypigeon", cards );
 }
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ client/src/test/app.js
@@ -0,0 +1,71 @@
+var tmi = require( 'tmi.js' );
+var Calculator = require( './models/Calculator' );
+var Cards = require( './models/Cards' );
+
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: 'aws',
+        reconnect: true
+    },
+    identity: {
+        username: "RoboBleepBloop",
+        password: "oauth:if3e2ziho9n2bdoy053vie45w2g1t3"
+    },
+    channels: [ 'skinnypigeon' ]
+};
+
+var client = new tmi.client(options);
+
+client.connect();
+
+client.on( 'connected', function( address, port ) {
+    client.action( "skinnypigeon I am connected to your chat" );
+})
+
+// client.on( 'chat', function( channel, user, message, self ) {
+//     var calc = new Calculator();
+//     calc.sum( message );
+//     var answer = calc.equals();
+//     client.action( "skinnypigeon", answer );
+//     console.log( answer );
+// });
+
+// client.on( 'chat', function( channel, user, message, self ) {
+//     var pack = new Cards();
+//     var card1 = pack.deal();
+//     var card2 = pack.deal();
+//     var cards = card1 + card2;
+
+//     client.action( "skinnypigeon", cards );
+// });
+
+client.on("message", function (channel, userstate, message, self) {
+    if (self) return;
+
+    switch(userstate["message-type"]) {
+        case "action":
+            break;
+        case "chat":
+            if( message === "!deal" ) {
+                deal();
+        }
+            break;
+        case "whisper":
+            break;
+        default:
+            break;
+    }
+});
+
+var deal = function() {
+    var pack = new Cards();
+    var card1 = pack.deal();
+    var card2 = pack.deal();
+    var cards = card1 + card2;
+
+    client.action( "skinnypigeon", cards );
+}
+
