cd1f0ee8ce5104ea1fa91ee6473b6f543ac264b5
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-10 21:54:46 +0100

    adding the player model and the game can hopefully take bets

---
+++ client/src/models/Player.js
@@ -0,0 +1,20 @@
+var Player = function( name ) {
+  this.name = name;
+  this.chips = 500;
+  this.hand = [];
+  this.bet = 0;
+}
+
+Player.prototype = {
+  getCard: function( card ) {
+    this.hand.push( card )
+  },
+
+  placeBet: function( bet ) {
+    this.bet = bet;
+    this.chips -= this.bet;
+    return this.bet;
+  }
+}
+
+module.exports = Player;
