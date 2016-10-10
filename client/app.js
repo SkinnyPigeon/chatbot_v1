887aaa8f710c07ca283b175e2352e9130d582b8e
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-10 14:19:23 +0100

    can add the output of a constructors action, need to see how input can work

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
-var tmi = require("tmi.js");
+var tmi = require( 'tmi.js' );
+var Calc = require( './src/calc' );
 
 var options = {
     options: {
@@ -12,13 +13,20 @@ var options = {
         username: "robobleepbloop",
         password: "oauth:if3e2ziho9n2bdoy053vie45w2g1t3"
     },
-    channels: ['skinnypigeon']
+    channels: [ 'skinnypigeon' ]
 };
 
 var client = new tmi.client(options);
 
 client.connect();
 
+client.on( 'connected', function( address, port ) {
+    client.action( "skinnypigeon I am connected to your chat" );
+})
+
 client.on( 'chat', function( channel, user, message, self ) {
-    client.action( "skinnypigeon", user[ 'display-name' ] + " I am connected to your chat" );
+    var calc = new Calc();
+    var answer = calc.multi();
+    // client.action( "skinnypigeon", user[ 'display-name' ] + " I am connected to your chat" );
+    client.action( "skinnypigeon", answer );
 });
