8d70c357830b258ed3df589c927b11f8019d70f8
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-13 10:25:01 +0100

    finished the switch statement

--- client/src/models/Logic.js
+++ client/src/models/Logic.js
@@ -16,11 +16,45 @@ Logic.prototype = {
     }
   },
 
-  score: function( card ) {
+  setScore: function( card ) {
+    this.total = 0;
     switch( card ) {
       case "2":
-      this.total += 2;
-      break
+        this.total += 2;
+        break
+      case "3":
+        this.total += 3;
+        break
+      case "4":
+        this.total += 4;
+        break
+      case "5":
+        this.total += 5;
+        break
+      case "6":
+        this.total += 6;
+        break
+      case "7":
+        this.total += 7;
+        break
+      case "8":
+        this.total += 8;
+        break
+      case "9":
+        this.total += 9;
+        break
+      case "1":
+        this.total += 10;
+        break
+      case "J":
+        this.total += 10;
+        break
+      case "Q":
+        this.total += 10;
+        break
+      case "K":
+        this.total += 10;
+        break
     }
   }
 }
