039c1eab55088158f70f59ff9ccc39a1774bb2c6
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-10-13 10:21:06 +0100

    trying the score setter with a switch statement

--- client/src/models/Logic.js
+++ client/src/models/Logic.js
@@ -1,20 +1,28 @@
 var Logic = function( cards ) {
   this.cards = cards;
-  this.total = 
+  this.total = 0;
+  this.aceHigh = 0;
+  this.aceLow = 0;
 }
 
 Logic.prototype = {
 
   handleAces: function() {
-
+    for( var i = 0; i < this.cards.length; i++ ) {
+      if( this.cards[i].chatAt(0) === "A" ) {
+        this.aceHigh = 11;
+        this.aceLow = 1;
+      }
+    }
   },
 
-  score: function() {
-    for( var i = 0; i < this.cards.length; i++ ) {
-
+  score: function( card ) {
+    switch( card ) {
+      case "2":
+      this.total += 2;
+      break
     }
   }
-
 }
 
 module.exports = Logic;
